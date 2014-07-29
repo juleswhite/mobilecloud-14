85c510061228e514519b2846e85675f7be00835f
korest <crazyorest@gmail.com>
2014-07-30 00:11:24 +0300

    Home added basic controller

--- assignments/Asgn1-VideoUp/src/main/java/org/magnum/dataup/VideoController.java
+++ assignments/Asgn1-VideoUp/src/main/java/org/magnum/dataup/VideoController.java
@@ -22,34 +22,79 @@ import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.magnum.dataup.model.Video;
+import org.magnum.dataup.model.VideoStatus;
+import org.magnum.dataup.model.VideoStatus.VideoState;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Controller
 public class VideoController {
 
 	private List<Video> videos = new ArrayList<>();
-	
+
 	private AtomicLong id = new AtomicLong(1);
-	
+
 	@RequestMapping(value = VideoSvcApi.VIDEO_SVC_PATH, method = RequestMethod.GET)
 	public @ResponseBody Collection<Video> getVideoList() {
 		return videos;
 	}
-	
+
 	@RequestMapping(value = VideoSvcApi.VIDEO_SVC_PATH, method = RequestMethod.POST)
 	public @ResponseBody Video addVideo(@RequestBody Video v) {
-		if(v != null) {
+		if (v != null) {
 			v.setId(id.getAndIncrement());
-			v.setDataUrl("url");
-			if(videos.add(v)) {
+			v.setDataUrl(getDataUrl(v.getId()));
+			if (videos.add(v)) {
 				return v;
 			}
 		}
 		return v;
+	}
+
+	@RequestMapping(value = VideoSvcApi.VIDEO_DATA_PATH, method = RequestMethod.GET)
+	public @ResponseBody Video getData(@PathVariable("id") long videoId) {
+		for (int i = 0; i < videos.size(); i++) {
+			if (Long.compare(videos.get(i).getId(), videoId) == 0) {
+				return videos.get(i);
+			}
+		}
+		throw new ResourceNotFoundException();
+	}
+	
+	@RequestMapping(value = VideoSvcApi.VIDEO_DATA_PATH, method = RequestMethod.POST)
+	public @ResponseBody VideoStatus setVideoData(@PathVariable("id") long videoId) {
+		Video video = addVideo(new Video());
+		return new VideoStatus(VideoState.READY);
+	}
+
+	@ResponseStatus(value = org.springframework.http.HttpStatus.NOT_FOUND)
+	public static final class ResourceNotFoundException extends
+			RuntimeException {
+		private static final long serialVersionUID = -8916205905961324734L;
+	}
+	
+	private String getDataUrl(long videoId){
+        String url = getUrlBaseForLocalServer() + "/video/" + videoId + "/data";
+        return url;
+    }
+
+	private String getUrlBaseForLocalServer() {
+		HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder
+				.getRequestAttributes()).getRequest();
+		String base = "http://"
+				+ request.getServerName()
+				+ ((request.getServerPort() != 80) ? ":"
+						+ request.getServerPort() : "");
+		return base;
 	}
 }
