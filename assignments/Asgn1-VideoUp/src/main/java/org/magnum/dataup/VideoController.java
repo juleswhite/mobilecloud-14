35c1aefe176e5afc5e511a738a1c9c1f108fcfa3
unknown <kir.maxim@gmail.com>
2014-08-11 23:23:29 +0300

    methods implementation

--- assignments/Asgn1-VideoUp/src/main/java/org/magnum/dataup/VideoController.java
+++ assignments/Asgn1-VideoUp/src/main/java/org/magnum/dataup/VideoController.java
@@ -17,37 +17,83 @@
  */
 package org.magnum.dataup;
 
-import java.util.List;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.magnum.dataup.model.Video;
+import org.magnum.dataup.model.VideoStatus;
+import org.magnum.dataup.model.VideoStatus.VideoState;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class VideoController {
 
 	private static final Logger logger = LoggerFactory.getLogger(VideoController.class);
 	
+	/**
+	 * Used to generate unique id's to movies.
+	 */
+	private final AtomicLong idGenerator = new AtomicLong(0);
+	
+	/**
+	 * A dictionary that holds video records.
+	 */
+	private Map<Long,Video> videos = new ConcurrentHashMap<>();
+	
 	@RequestMapping(value="/video", method=RequestMethod.GET)
-	public List<Video> getVideos() {
+	public @ResponseBody Collection<? extends Video> getVideos() {
 		logger.debug("Retrieving videos");
-		return null;
+		return videos.values();
 	}
 	
 	@RequestMapping(value="/video", method=RequestMethod.POST)
-	public void addVideo(
-			Video v
-			){
-		logger.debug("Adding video");
-		
+	public @ResponseBody Video addVideo(@RequestBody Video v){
+		long id = idGenerator.incrementAndGet();
+		String dataUrl = getDataUrl(id);
+		if (v.getId() == 0){
+			v.setId(id);
+		}
+		v.setDataUrl(dataUrl);
+		videos.put(v.getId(), v);
+		logger.debug("Successfully added meta data for video, id: {}", id);
+		return v;
 	}
 	
 	@RequestMapping(value="/video/{id}/data", method=RequestMethod.POST)
-	public void uploadVideo(){
-		logger.debug("Uploading video");
+	public @ResponseBody VideoState uploadVideo(
+			@RequestParam("id") Long id,
+			@RequestParam("data") MultipartFile videoData,
+			HttpServletResponse response){
+		logger.debug("Starting to upload video data");
+		if (!videos.containsKey(id)){
+			response.setStatus(404);
+			return null;
+		}
+		try(InputStream in = videoData.getInputStream()){
+			//handle the input stream
+			
+		} catch (IOException e) {
+			logger.error("Failed to upload a file to the server, due to:", e);
+			response.setStatus(404);
+		}
+		return VideoState.READY;
 	}
 	
 	
@@ -55,5 +101,20 @@ public class VideoController {
 	public void downloadVideo(){
 		logger.debug("Downloading video");
 	}
+	
+    private String getDataUrl(long videoId){
+        String url = getUrlBaseForLocalServer() + "/video/" + videoId + "/data";
+        return url;
+    }
+    
+    
+    private String getUrlBaseForLocalServer() {
+        HttpServletRequest request = 
+            ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        String base = 
+           "http://"+request.getServerName() 
+           + ((request.getServerPort() != 80) ? ":"+request.getServerPort() : "");
+        return base;
+     }
 	
 }
