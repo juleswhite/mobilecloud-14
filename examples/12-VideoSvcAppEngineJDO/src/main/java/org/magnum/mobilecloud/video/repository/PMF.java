e2b117bb59d8ab8f757cd0b448f9dd3a63f62b1c
Jules White <jules.white@gmail.com>
2014-08-30 17:09:02 -0500

    - Initial commit of AppEngine example

---
+++ examples/12-VideoSvcAppEngineJDO/src/main/java/org/magnum/mobilecloud/video/repository/PMF.java
@@ -0,0 +1,38 @@
+package org.magnum.mobilecloud.video.repository;
+
+/*************************************************************************
+* Copyright 2010 Jules White
+* 
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at http://www.apache.org/licenses/
+* LICENSE-2.0 Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an "AS IS"
+* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+* implied. See the License for the specific language governing permissions
+* and limitations under the License.
+**************************************************************************/
+
+import javax.jdo.JDOHelper;
+import javax.jdo.PersistenceManagerFactory;
+
+/**
+ * Standard code for JDO on AppEngine. See:
+ * https://developers.google.com/appengine/docs/java/datastore/jdo/overview-dn2
+ * 
+ * 
+ * 
+ * @author jules
+ *
+ */
+public final class PMF {
+	private static final PersistenceManagerFactory pmfInstance = JDOHelper
+			.getPersistenceManagerFactory("transactions-optional");
+
+	private PMF() {
+	}
+
+	public static PersistenceManagerFactory get() {
+		return pmfInstance;
+	}
+}
---
+++ examples/12-VideoSvcAppEngineJDO/src/main/java/org/magnum/mobilecloud/video/repository/Video.java
@@ -0,0 +1,103 @@
+/* 
+ **
+ ** Copyright 2014, Jules White
+ **
+ ** 
+ */
+package org.magnum.mobilecloud.video.repository;
+
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+import com.google.common.base.Objects;
+
+@PersistenceCapable
+public class Video {
+
+	@PrimaryKey
+	@Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+	private Long id;
+
+	@Persistent
+	private String name;
+	
+	@Persistent
+	private String url;
+	
+	@Persistent
+	private long duration;
+
+	public Video() {
+	}
+
+	public Video(String name, String url, long duration) {
+		super();
+		this.name = name;
+		this.url = url;
+		this.duration = duration;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public long getDuration() {
+		return duration;
+	}
+
+	public void setDuration(long duration) {
+		this.duration = duration;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * Two Videos will generate the same hashcode if they have exactly the same
+	 * values for their name, url, and duration.
+	 * 
+	 */
+	@Override
+	public int hashCode() {
+		// Google Guava provides great utilities for hashing
+		return Objects.hashCode(name, url, duration);
+	}
+
+	/**
+	 * Two Videos are considered equal if they have exactly the same values for
+	 * their name, url, and duration.
+	 * 
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof Video) {
+			Video other = (Video) obj;
+			// Google Guava provides great utilities for equals too!
+			return Objects.equal(name, other.name)
+					&& Objects.equal(url, other.url)
+					&& duration == other.duration;
+		} else {
+			return false;
+		}
+	}
+
+}
